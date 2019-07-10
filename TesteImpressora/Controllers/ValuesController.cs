a42f71a8cd2e1f77100396c2fe395fff575e3563
Fernando Mendes <Fernando@BRNSPN153730.br.natura>
2019-07-10 20:21:52 -0300

    te

---
+++ TesteImpressora/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThermalDotNet;
+
+namespace TesteImpressora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var printerPort = new SerialPort("/dev/ttyAMA0", 19200);
+            printerPort.Open();
+            var printer = new ThermalPrinter(printerPort);
+            printer.WakeUp();
+            printer.WriteLine("TESTE DE IMPRESSÃO");
+            printer.LineFeed();
+            printer.Sleep();
+            printerPort.Close(); 
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
